503b7c54ed188028d3ed4abd60c72b724b034d70
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2020-05-20 18:19:28 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,17 @@ readme = ''
 setup(
     long_description=readme,
     name='simple-back',
-    version='0.1.0',
+    version='0.2.0',
     python_requires='==3.*,>=3.7.0',
     author='Christoph Minixhofer',
     author_email='christoph.minixhofer@gmail.com',
     packages=['simple_back'],
     package_dir={"": "."},
     package_data={},
-    install_requires=['requests-html==0.*,>=0.10.0', 'yahoo-fin==0.*,>=0.8.5'],
+    install_requires=[
+        'numpy==1.*,>=1.18.4', 'pandas==1.*,>=1.0.3',
+        'pandas-market-calendars==1.*,>=1.3.5', 'requests-html==0.*,>=0.10.0',
+        'yahoo-fin==0.*,>=0.8.5'
+    ],
     extras_require={"dev": ["pytest==5.*,>=5.2.0"]},
 )
