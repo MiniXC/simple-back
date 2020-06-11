955a25ef52f350cf3f61446e93a21cb5d50725ac
MiniXC <MiniXC@users.noreply.github.com>
2020-06-11 10:56:46 +0000

    build

--- docs/scripts/replace_nbsphinx_ref.py
+++ docs/scripts/replace_nbsphinx_ref.py
@@ -17,28 +17,31 @@ def find_ref(x):
         if r.endswith(x):
             return r
 
-link = re.compile('\[`([^\]`]*)`\][^(]')
 
-for file in glob('../../docs/**/*.ipynb', recursive=True):
-    if '_build' not in file:
+link = re.compile("\[`([^\]`]*)`\][^(]")
+
+for file in glob("../../docs/**/*.ipynb", recursive=True):
+    if "_build" not in file:
         print(file)
         with open(file) as api:
             j_file_content = json.loads(api.read())
-            for i, cell in enumerate(j_file_content['cells']):
-                if cell['cell_type'] == 'markdown':
-                    for j, line in enumerate(cell['source']):
+            for i, cell in enumerate(j_file_content["cells"]):
+                if cell["cell_type"] == "markdown":
+                    for j, line in enumerate(cell["source"]):
                         for match in link.findall(line):
-                            level = file.split('docs/')[1].count('/')
-                            base = ''.join(['../']*level)
-                            base += 'api/simple_back.html'
-                            anchor = find_ref(match.replace('()', ''))
+                            level = file.split("docs/")[1].count("/")
+                            base = "".join(["../"] * level)
+                            base += "api/simple_back.html"
+                            anchor = find_ref(match.replace("()", ""))
                             class_str = find_ref(match)
-                            to_replace = ('<a class="reference internal"',
-                                         f' href="https://simple-back.readthedocs.io/en/latest/api/simple_back.html#{class_str}"',
-                                         f' title="{class_str}">',
-                                          '<code class="xref py py-class docutils literal notranslate">',
-                                         f'<span class="pre">{match}</span></code></a>')
-                            line = line.replace(f'[`{match}`]', ''.join(to_replace))
-                            j_file_content['cells'][i]['source'][j] = line
-            with open(file, 'w') as outfile:
+                            to_replace = (
+                                '<a class="reference internal"',
+                                f' href="https://simple-back.readthedocs.io/en/latest/api/simple_back.html#{class_str}"',
+                                f' title="{class_str}">',
+                                '<code class="xref py py-class docutils literal notranslate">',
+                                f'<span class="pre">{match}</span></code></a>',
+                            )
+                            line = line.replace(f"[`{match}`]", "".join(to_replace))
+                            j_file_content["cells"][i]["source"][j] = line
+            with open(file, "w") as outfile:
                 json.dump(j_file_content, outfile)
