dfadf1fbc3d2baf2eb9f188867488f76593b2bb8
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2020-06-21 11:34:31 +0200

    0.5.5

--- simple_back/__init__.py
+++ simple_back/__init__.py
@@ -7,7 +7,7 @@ code, while avoiding time leaks.
 """
 
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 
 from . import backtester
 from . import strategy
--- tests/test_simple_back.py
+++ tests/test_simple_back.py
@@ -3,7 +3,7 @@ from simple_back.backtester import BacktesterBuilder
 
 
 def test_version():
-    assert __version__ == "0.5.4"
+    assert __version__ == "0.5.6"
 
 
 def test_compare_quantopian():
