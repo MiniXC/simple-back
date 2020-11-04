9da3e10d3333e0ec68c9017dadd8e9c6dc5ab908
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2020-11-04 20:06:16 +0000

    fix iso format

--- tests/test_simple_back.py
+++ tests/test_simple_back.py
@@ -18,7 +18,7 @@ def test_compare_quantopian():
     )
     bt = builder.build()
     bt.prices.clear_cache()
-    for _, _, b in bt[date.fromisoformat("2017-1-1") : date.fromisoformat("2020-1-1")]:
+    for _, _, b in bt[date.fromisoformat("2017-01-01") : date.fromisoformat("2020-01-01")]:
         if not b.pf or True:
             b.pf.liquidate()
             print(b._schedule.index)
